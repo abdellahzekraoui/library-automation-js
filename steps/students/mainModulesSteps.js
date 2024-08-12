54ab2d4845f8048416b67cd2e717a3c6f632c188
abdellahzekraoui <zekraouiabdell@gmail.com>
2024-08-12 16:07:20 -0400

    Main Modules Feature is completed and tested

--- steps/students/mainModulesSteps.js
+++ steps/students/mainModulesSteps.js
@@ -3,5 +3,15 @@ import { expect } from "@playwright/test";
 import { BrowserUtility } from "../../utilities/BrowserUtility.js";
 import { PageManager } from "../../globalPagesSetup.js";
 
-
 // WRITE YOUR STEP DEFINITIONS HERE...
+
+Then('user should see {string}  on left top corner of the screen', async function (string) {
+    const books = await PageManager.dashboardPage.bookLink.innerText();
+    expect(books).toBe(string);
+  });
+
+
+  Then('user should see {string} on left top corner of the screen', async function (string) {
+    const borrowingBooks = await PageManager.dashboardPage.borrowingBookLink.innerText();
+    expect(borrowingBooks).toBe(string);
+  });
